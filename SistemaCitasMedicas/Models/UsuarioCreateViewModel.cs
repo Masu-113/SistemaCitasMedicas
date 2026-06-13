a4b6425b835dc12e98911f6c7c472954fb59ab67
marlon <marlonsuarezbaltodano@gmail.com>
2026-06-13 14:34:59 -0600

    Se creo un nuevo modelo

---
+++ SistemaCitasMedicas/Models/UsuarioCreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace SistemaCitasMedicas.Models
+{
+    public class UsuarioCreateViewModel
+    {
+        // Usuario
+        public required string Nombre { get; set; }
+        public required string Apellido { get; set; }
+        public required string Correo { get; set; }
+        public required string PasswordHash { get; set; }
+        public string? Telefono { get; set; }
+        public bool Activo { get; set; }
+        public int IdRol { get; set; }
+
+        // Médico
+        public int? IdEspecialidad { get; set; }
+        public string? NumeroLicencia { get; set; }
+        public int? DuracionCitaMin { get; set; }
+
+        // Paciente
+        public DateOnly? FechaNacimiento { get; set; }
+        public string? Sexo { get; set; }
+        public string? Direccion { get; set; }
+    }
+}
