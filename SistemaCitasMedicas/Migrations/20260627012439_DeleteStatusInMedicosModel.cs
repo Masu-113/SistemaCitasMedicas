b04528e80e47fd4676b4b33b7cfe5ff6867b7759
marlon <marlonsuarezbaltodano@gmail.com>
2026-06-26 19:37:05 -0600

    Migracion realizada debido a cambios en el modelo de medicos.

---
+++ SistemaCitasMedicas/Migrations/20260627012439_DeleteStatusInMedicosModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaCitasMedicas.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteStatusInMedicosModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cedula",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "Activo",
+                table: "Medicos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cedula",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Activo",
+                table: "Medicos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
